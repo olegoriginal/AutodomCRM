bd40907a6d523ffd6bd72ac5946c647986bf97ff
Oleksii <lxvslnk@gmail.com>
2020-10-17 03:30:20 +0800

    fix static content

--- server/server.js
+++ server/server.js
@@ -18,12 +18,14 @@ app.use(
 )
 app.use(bodyParser.json({ limit: "50mb", extended: true }))
 
-app.use(express.static(path.join(__dirname, "build")))
+app.use(express.static(path.join(__dirname, "../build")))
 app.use('/api/v1/task', taskRoutes)
 
 
-app.get("/", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "../build", "index.html"))
 })
-app.listen(process.env.PORT || 9000)
+app.listen(process.env.PORT || 9000, () => {
+  console.log("listening on", process.env.PORT || 9000)
+})
 
